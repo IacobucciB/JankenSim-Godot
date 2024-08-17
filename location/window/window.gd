13af81e7b1442c8f92b35852275dcb104ed5a8af
Iacobucci Juan B. <link00222@gmail.com>
2024-08-17 14:59:28 -0300

    window

--- location/window/window.gd
+++ location/window/window.gd
@@ -1,1 +1,34 @@
 extends Node2D
+
+@onready var log_tlabel = $TextControl/LogTextLabel
+@onready var open_window_sprite = $OpenedWindowSprite2D
+@onready var closed_window_sprite = $ClosedWindowSprite2D
+
+var is_window_open = false
+
+func _on_ready():
+	randomize()
+
+func _on_hub_button_pressed():
+	pass # Replace with function body.
+
+func _on_move_away_button_pressed():
+	if is_window_open == true:
+		log_tlabel.add_text("You approached the window \n")
+		open_window_sprite.visible = true
+		closed_window_sprite.visible = false
+		is_window_open = false
+	else:
+		log_tlabel.add_text("You moved away from the window \n")
+		open_window_sprite.visible = false
+		closed_window_sprite.visible = true
+		is_window_open = true
+
+func _on_rock_button_pressed():
+	pass # Replace with function body.
+
+func _on_paper_button_pressed():
+	pass # Replace with function body.
+
+func _on_scissors_button_pressed():
+	pass # Replace with function body.
